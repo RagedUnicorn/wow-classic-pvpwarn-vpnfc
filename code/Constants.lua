f8f55f74e4e5879e960fefd94928d34810a64698
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2025-07-10 21:44:15 +0200

    fix: Update asset path for Night Female Classic voice pack

--- code/Constants.lua
+++ code/Constants.lua
@@ -26,5 +26,5 @@
 RGPVPW_VP_NFC_CONSTANTS = {
   ADDON_NAME = "PVPWarn_VoicePack_NFC",
   DISPLAY_NAME = "Night Female Classic",
-  ASSET_PATH = "Interface\\AddOns\\PVPWarn_VoicePack_NFC\\assets\\"
+  ASSET_PATH = "Interface\\AddOns\\PVPWarn_VoicePack_NFC\\assets\\sounds\\"
 }
